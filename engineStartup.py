9ac84edfcf3efd613bd8df5f85c0ac9d838a4de1
AlphaDevTeam <mihindugajaba@gmail.com>
2018-09-09 14:06:25 +0530

    Update engineStartup.py

--- engineStartup.py
+++ engineStartup.py
@@ -1,1 +1,46 @@
+import RPi.GPIO as GPIO
+import time
 
+GPIO.setmode(GPIO.BCM)
+
+pinList = [14,15,18,23,24] 
+
+for i in pinList:
+	GPIO.setup(i,GPIO.OUT)
+	GPIO.output(i,GPIO.HIGH)
+
+
+SleepTimeL =1
+EngineStartupDelay = 0.5
+
+try:
+
+  for i in pinList:
+    if i == 24:
+      print "Engine ON ..." , varRelay
+		  GPIO.output(int(varRelay),GPIO.LOW)
+      time.sleep(EngineStartupDelay);
+		  print "Engine ON ..." + str(varRelay) + ".... OK"
+    else:
+      print "Turning ON ..." , varRelay
+			GPIO.output(int(varRelay),GPIO.LOW)
+			time.sleep(SleepTimeL);
+      print "Turning ON ..." + str(varRelay) + ".... OK"
+    
+  while True:
+		varRelay = input('Please enter a number between 1-8 : ') 
+		varCommand = raw_input('Please enter Command : ').lower().split(' ')
+		print "Command received ..." , varCommand[0]
+		if varCommand[0] == "on":
+			print "Turning ON ..." , varRelay
+			GPIO.output(int(varRelay),GPIO.LOW)
+			print "Turning ON ..." + str(varRelay) + ".... OK"
+		else: 
+			print "Turning OFF ..." , varRelay
+			GPIO.output(int(varRelay),GPIO.HIGH)
+			print "Turning OFF ..." + str(varRelay) + ".... OK"
+		  time.sleep(SleepTimeL);
+
+except KeyboardInterrupt:
+	print "Quit"
+	GPIO.cleanup()
