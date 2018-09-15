277f2da6ac4776812b97838ccf8967012107eef2
AlphaDevTeam <mihindugajaba@gmail.com>
2018-09-16 02:41:42 +0530

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,10 +1,10 @@
 import time
-import Serial
+import serial
 
 recipient = "+94777117477"
 message = "Hello, World!"
 
-port = Serial.Serial("/dev/ttyS0", baudrate=115200, timeout=5.0)
+port = serial.Serial("/dev/ttyS0", baudrate=115200, timeout=5.0)
 try:
     time.sleep(0.5)
     phone.write(b'ATZ\r')
